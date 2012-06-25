4cf3f8a5dd19aecbf12167c78ef3c45129628bf1
vivek-prakash <vivekprakash86@gmail.com>
2012-06-26 01:11:07 +0530

    Demonstrating How Thing Behave On Function Overriding

---
+++ testInheritance.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+class Base
+{
+public:
+    void test(int value=10)
+    {
+        printf("%d\n",value);
+    }
+};
+
+class DerivedChangeDefault : public Base
+{
+public:
+    void test(int value=20)
+    {
+        printf("%d\n",value);
+    }
+};
+
+class DerivedChangeProtection : public Base
+{
+protected:
+    void test(int value=10)
+    {
+        printf("Derived: %d\n",value);
+    }
+};
+
+int main()
+{
+    Base* b1 = new DerivedChangeDefault;
+    Base* b2 = new DerivedChangeProtection;
+
+    b1->test();
+    b2->test();
+
+    ((DerivedChangeDefault*)b1)->test();
+#if 0
+    ((DerivedChangeProtection*)b2)->test();
+#endif
+
+    return 0;
+}
