8dff1d8ebbb940f06fc43d0d0379cb1399111a4e
vivek-prakash <vivekprakash86@gmail.com>
2012-06-27 23:27:30 +0530

    Updating to add Integer class

--- incrementOprs.cpp
+++ incrementOprs.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 using namespace std;
 
+class Integer
+{
+public:
+    Integer(int v = 0) : _val(v) { }
+    Integer& operator++()
+    {
+        ++_val;
+        return *this;
+    }
+
+    Integer operator++(int)
+    {
+        Integer ans = (*this);
+        ++(*this);
+        return ans;
+    }
+    friend ostream& operator<<(ostream& os, const Integer& i);
+private:
+    int _val;
+};
+
+ostream& operator<<(ostream& os, const Integer& i)
+{
+    os<<i._val;
+}
+
 int main()
 {
-    int k = 20;
+    Integer k = 20;
     (++(++k));
 
     cout<<k;
