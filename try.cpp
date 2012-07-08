76f28b7e72104b9665d7d9c109f81ab37c040600
root <root@localhost.localdomain>
2012-07-09 00:03:02 +0400

    basic2

--- try.cpp
+++ try.cpp
@@ -8,55 +8,49 @@ using namespace std;
 template <typename _T>
 class Stack
 {
-public:
-    Stack() : v1(0) { }
-	~Stack()
-	{
-while(v1.size!=0)
-v1.pop_back();
-	}
+	public:
+		Stack() { }
+		void push(const _T& elem)
+		{
+			v1.push_back(elem);
+		}
 
-	void push(const _T& elem)
-	{
-v1.push_back(elem);
-	}
+		void pop()
+		{
+			v1.pop_back();
+		}
 
-	void pop()
-	{
-v1.pop_back();
-	}
+		const _T& top() const
+		{
+			return v1[v1.size()-1];
+		}
 
-	const _T& top() const
-	{
-return v1[v1.size()-1];
-	}
+		bool empty() const
+		{
+			return (v1.front() == NULL);
+		}
 
-	bool empty() const
-	{
-		return (v1.front() == NULL);
-	}
-
-private:
-vector<_T> v1;
+	private:
+		vector<_T> v1;
 };
 
 int main()
 {
-/*	Stack<int> s;
-	int d;
-	while(cin>>d)
-	{
+	/*	Stack<int> s;
+		int d;
+		while(cin>>d)
+		{
 		s.push(d);
-	}
-	while(!s.empty())
-	{
+		}
+		while(!s.empty())
+		{
 		cout<<s.top()<<endl;
 		s.pop();
-	}
-*/
+		}
+		*/
 	cout<<"problem sir";
-/*#ifdef WIN32
-	system("PAUSE");
+	/*#ifdef WIN32
+	  system("PAUSE");
 #endif // WIN32
 	return 0;*/
 }
