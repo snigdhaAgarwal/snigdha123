70fab8489d2aaae1d85044396d072957dc60452e
Vivek Prakash <vivekprakash86@gmail.com>
2012-07-09 19:04:09 +0530

    Demonstrating Template Specialization

--- gener-arr.cpp
+++ gener-arr.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
 template <class _T>
 void print(_T *arr,int size)
 {
@@ -11,6 +12,18 @@ void print(_T *arr,int size)
 		i++;
 	}
 }
+
+template <>
+void print<char>(char *arr,int size)
+{
+	int i=0;
+	while(i < size && arr[i] != '\0')
+	{
+		cout<<arr[i];
+		i++;
+	}
+}
+
 int main()
 {
 //	vector<char> arr;
@@ -18,4 +31,7 @@ int main()
 //		cin>>arr[i];
 	char arr[]={'a', 'b', 'c', 'd', 'e' };	
 	print(arr,5);
+	int arr1[] = { 1, 2, 3, 4, 5 };
+	print(arr1,5);
+	cout<<endl;
 }
