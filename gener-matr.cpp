fe53a695c5aefbc3642a91725de74e521a005293
root <root@localhost.localdomain>
2012-07-10 00:58:31 +0400

    g

--- gener-matr.cpp
+++ gener-matr.cpp
@@ -6,7 +6,7 @@ class matrix
 	public:
 		matrix(int x,int y):x(x),y(y)
 		{
-			T mat=new T*[x];
+			mat=new T*[x];
 			for(int i=0;i<x;i++)
 			{
 				mat[i] = new T[y];
@@ -14,8 +14,18 @@ class matrix
 					mat[i][j]=0;
 			}
 		}
+		matrix<T> operator+(const matrix<T>& m) const
+		{
+			matrix<T> m2(m.x,m.y);
+			for(int i=0;i<m.x;i++)
+				for(int j=0;j<m.y;j++)
+				{
+					m2.mat[i][j]=m.mat[i][j]+mat[i][j];
+				}
+			return m2;
+		}
 		template<typename T1>
-		friend std::istream& operator>>(std::istream& is, const matrix<T1>& m);
+			friend std::istream& operator>>(std::istream& is, const matrix<T1>& m);
 	private:
 		T **mat;
 		int x,y;
@@ -34,5 +44,9 @@ std::istream& operator>>(std::istream& is, const matrix<T>& m)
 int main()
 {
 	matrix<int> m(2,3);
+	matrix<int> m1(2,3);
+	matrix<int> m2(2,3);
 	cin>>m;
+	cin>>m1;
+	m2=m+m1;
 }
