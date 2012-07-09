9a3926754e07278d2d406b14df93661238cfdeac
root <root@localhost.localdomain>
2012-07-10 00:11:07 +0400

    g

--- gener-matr.cpp
+++ gener-matr.cpp
@@ -4,9 +4,11 @@ template <typename T>
 class matrix
 {
 	public:
-		matrix(int x, int y)
+		matrix(int x,int y)
 		{
 			T **mat=new T*[x];
+			x=x;
+			y=y;
 			for(int i=0;i<x;i++)
 			{
 				mat[i] = new T[y];
@@ -14,11 +16,20 @@ class matrix
 					mat[i][j]=0;
 			}
 		}
-
+		friend std::istream& operator>>(std::istream& is, const matrix<T>& m);
 	private:
 		T **mat;
+		int x,y;
 };
+std::istream& operator>>(std::istream& is, const matrix<T>& m)
+{
+	for(int i=0;i<m.x;i++)
+		for(int j=0;j<m.y;j++)
+			is>>m.mat[i][j];
+	return is;
+}
 int main()
 {
 	matrix<int> m(2,3);
+	cin>>m;
 }
