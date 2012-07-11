f96a7c6023a0d9d1a1191fd05fb01d42291aaef6
vivek-prakash <vivekprakash86@gmail.com>
2012-07-12 02:57:01 +0530

    Adding All Functions

--- free_mem.cpp
+++ free_mem.cpp
@@ -5,6 +5,7 @@ class pointer
 {
 	public:
 		pointer(t *p1) : p(p1) {}
+        pointer(pointer& p) : p(p.release()) { }
 		~pointer()
 		{
 			delete p;
@@ -13,19 +14,45 @@ class pointer
 		{
 			return p;
 		}
-		const pointer& operator=(t *p1)
+		pointer& operator=(t *p1)
 		{
 			p=p1.p;
-			return (* this);
+			return (*this);
 		}
+        pointer& operator=(pointer& p1)
+        {
+            p=p1.release();
+        }
+        t *release()
+        {
+            t *temp=p;
+            p=NULL;
+            return temp;
+        }
 		t& operator*()
 		{
 			return (*p);
 		}
+
+        template <typename T>
+        friend ostream& operator<<(ostream& os, const pointer<T>& p);
 	private:
 		t *p;
 };
+
+template <typename T>
+ostream& operator<<(ostream& os, const pointer<T>& p)
+{
+    os<<p.p;
+    return os;
+}
+
 int main()
 {
 	pointer<int> p=new int;
+    (*p) = 30;
+    pointer<int> p1 = p;
+    cout<<(*p1)<<endl;
+    cout<<p<<endl;
+    return 0;
 }
