7707d4df53a715eb8e0245f20f9ad8f912889120
root <root@localhost.localdomain>
2012-07-12 18:49:13 +0400

    prob

--- recursive.cpp
+++ recursive.cpp
@@ -1,5 +1,23 @@
 #include<iostream>
 using namespace std;
+int gcd(int a,int b)
+{
+    if(a>b)
+    {
+        if(a%b==0)
+            return b;
+        else 
+            return gcd(a,a%b);
+    }
+    else 
+    {
+        if(b%a==0)
+            return a;
+        else
+            return gcd(b%a,a);
+
+    }
+}
 int ncr(int n,int r)
 {
     if(r>n)
@@ -12,4 +30,6 @@ int main()
 {
     int x=ncr(5,3);
     cout<<x<<endl;
+    int p=gcd(12,42);
+    cout<<p<<endl;
 }
